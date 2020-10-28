75c18607755ded65a5d5f7b241eddb88877e8477
nhatmn2 <38064467+nhatmn2@users.noreply.github.com>
2020-10-28 13:48:23 -0700

    this is for the new branch I'm testing

--- src/com/company/Zork.java
+++ src/com/company/Zork.java
@@ -92,7 +92,7 @@ class Room extends MapSite {
     //constructor for room
     public Room() {
         roomNumber = roomCount++;
-        System.out.println("creating room number " + roomNumber);
+        //System.out.println("creating room number " + roomNumber);
     }
 
     //method setSide() inside Room
@@ -109,7 +109,7 @@ class Room extends MapSite {
         else if (d == Direction.West){
             westSide = site;
         }
-        System.out.println("setting " + d.toString() + " side of " + this.toString() + " to " + site.toString());
+        //System.out.println("setting " + d.toString() + " side of " + this.toString() + " to " + site.toString());
     }
 
     //method getSide() inside Room
@@ -135,7 +135,7 @@ class Room extends MapSite {
     }
 
     void enter(){
-        System.out.println("enter to Room " + roomNumber);
+        //System.out.println("enter to Room " + roomNumber);
     }
 
 }
@@ -150,7 +150,7 @@ class Wall extends MapSite{
     //constructor for wall
     public Wall(){
         wallNumber = wallCount++;
-        System.out.println("creating Wall number" +  Integer.toString(wallNumber));
+        //System.out.println("creating Wall number" +  Integer.toString(wallNumber));
     }
     //method toString() inside Wall
     public String toString(){
@@ -175,7 +175,7 @@ class Door extends MapSite{
     //constructor for door
     public Door(Room r1, Room r2){
         doorNumber = doorCount++;
-        System.out.println("creating a door number " + doorNumber + " between " + r1 + " and " + r2);
+        //System.out.println("creating a door number " + doorNumber + " between " + r1 + " and " + r2);
         this.room1 = r1;
         this.room2 = r2;
     }
@@ -343,18 +343,6 @@ class LivingRoom extends Room implements WordParser {
     }
 }
 
-//=====================================================================================================
-//create livingRoom's door
-//=====================================================================================================
-//
-//class LivingRoomDoor extends Door{
-//    LivingRoomDoor (Room r1, Room r2){
-//        super(r1, r2);
-//    }
-//    public String toString() {
-//        return super.toString();
-//    }
-//}
 
 //=====================================================================================================
 //create Kitchen
@@ -482,8 +470,8 @@ class GrassyFields extends Room implements WordParser{
         //still trying to figure out how that is going to work
         //player class can be updated in this function to keep track of process
         if(words.contains("OPEN") && words.contains("DOOR")) {
-            ZorkUnderground zorkUnderground = new ZorkUnderground(); 
-            LivingRoom livingRoom = new LivingRoom();
+            ZorkUnderground zorkUnderground = new ZorkUnderground();
+            LivingRoom livingRoom = new LivingRoom(zorkUnderground);
             //create another starting point in the living room class
         }
         else {
